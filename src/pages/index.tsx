9b03123fbdbd22b6c30c84505d51f1da9b9c2d67
Yeonggwan Han <66952465+YeongMalon@users.noreply.github.com>
2022-04-30 11:25:00 +0900

    new package manager change (#57)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,5 @@
 import type { NextPage } from "next";
-import { useAuth } from "src/context/auth";
 
-const Home: NextPage = () => {
-  const { loggedIn, signInWithGoogle } = useAuth();
-
-  return (
-    <div>
-      <button type="button" onClick={signInWithGoogle}>
-        로그인
-      </button>
-      <div>
-        {loggedIn ? <span>로그인 완료</span> : <span>현재 미로그인</span>}
-      </div>
-    </div>
-  );
-};
+const Home: NextPage = () => <div />;
 
 export default Home;
