09776a4ff1d1535a67fe2ce70804b1bf94514dd5
lifeisegg <58962402+lifeisegg123@users.noreply.github.com>
2022-05-04 19:40:14 +0900

    chore: add react-query & recoil (#60)

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,18 @@
 import type { AppProps } from "next/app";
+import { Hydrate, QueryClientProvider } from "react-query";
+import { RecoilRoot } from "recoil";
+import { queryClient } from "src/queryClient";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <RecoilRoot>
+      <QueryClientProvider client={queryClient}>
+        <Hydrate state={pageProps.dehydratedState}>
+          <Component {...pageProps} />
+        </Hydrate>
+      </QueryClientProvider>
+    </RecoilRoot>
+  );
 }
 
 export default MyApp;
