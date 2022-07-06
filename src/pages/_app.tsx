bf685217d3eeb105d2b34058a69a7af31551de6e
lifeisegg123 <leejj2002@naver.com>
2022-07-07 02:42:10 +0900

    chore: webview css properties

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import { Hydrate, QueryClientProvider } from "react-query";
 import { RecoilRoot } from "recoil";
 
@@ -5,6 +6,7 @@ import { useInit } from "src/hooks";
 import { queryClient } from "src/queryClient";
 
 import type { AppProps } from "next/app";
+
 import "src/ui/global.css";
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
@@ -13,6 +15,12 @@ const MyApp = ({ Component, pageProps }: AppProps) => {
     <QueryClientProvider client={queryClient}>
       <Hydrate state={pageProps.dehydratedState}>
         <RecoilRoot>
+          <Head>
+            <meta
+              content="width=device-width, initial-scale=1, maximum-scale=1.0, minimum-scale=1, user-scalable=0, viewport-fit=cover"
+              name="viewport"
+            />
+          </Head>
           <Component {...pageProps} />
         </RecoilRoot>
       </Hydrate>
