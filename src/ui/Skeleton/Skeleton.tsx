d15e32f86df51cde720e8f5838c3771b42f887e2
EUNYUSEO <eyseo328@gmail.com>
2022-07-17 18:48:43 +0900

    update: Skeleton Component

--- src/ui/Skeleton/Skeleton.tsx
+++ src/ui/Skeleton/Skeleton.tsx
@@ -1,27 +1,11 @@
 import { forwardRef } from 'react';
 
-import clsx, { ClassValue } from 'clsx';
-
 import { BoxProps, Box } from '../Box';
-import { base } from '../reset.css';
-import { sprinkles, Sprinkles } from '../sprinkles.css';
 
 import { skeletonCss } from './Skeleton.css';
 
-export interface SkeletonProps extends BoxProps, Pick<BoxProps, 'className' | 'height' | 'width' | 'borderRadius' | 'display' | 'flexDirection' | 'padding' | 'margin'>, Sprinkles {
-  className?: ClassValue;
-}
+export type SkeletonProps = Pick<BoxProps, 'as' | 'className' | 'height' | 'width' | 'borderRadius' | 'padding' | 'margin'>;
 
-const Skeleton = forwardRef(({ ...props }: SkeletonProps) => {
-  const styles = clsx(
-    sprinkles({
-      ...props,
-    }),
-    skeletonCss,
-    base
-  );
-
-  return <Box className={styles} />;
-});
+const Skeleton = forwardRef(({ className, ...props }: SkeletonProps) => <Box className={[skeletonCss, className]} {...props} />);
 
 export default Skeleton;
