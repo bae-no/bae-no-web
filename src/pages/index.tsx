a06086a4e8ca1fbaae8c5adacd07668237417c5c
lifeisegg <58962402+lifeisegg123@users.noreply.github.com>
2022-07-24 22:13:55 +0900

    feat: 헤더 컴포넌트 개발 (#122)
    * feat: 헤더 컴포넌트 개발

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,33 +1,45 @@
-import { keys, map, pipe, toArray } from '@fxts/core';
+import { keys, map, pipe, toArray } from "@fxts/core";
 
-import { AsyncBoundary } from 'src/components/AsyncBoundary';
-import { Typography, Toggle, Popup, BottomTab, Skeleton } from 'src/ui';
-import Icon from 'src/ui/Icon/Icon';
-import { SVG_ICON_MAP } from 'src/ui/Icon/iconMap';
+import { AsyncBoundary } from "src/components/AsyncBoundary";
+import { Typography, Toggle, Popup, Skeleton } from "src/ui";
+import Icon from "src/ui/Icon/Icon";
+import { SVG_ICON_MAP } from "src/ui/Icon/iconMap";
+import Layout from "src/ui/Layout/Layout";
 
 const LoadingComponent = () => <h1>...loading</h1>;
 const ErrorFallback = () => (
   <div>
     <Typography fontSize="body2-b">test</Typography>
     <Toggle />
-    <Popup buttonDirection="column" cancelText="tesaaaa" confirmText="test" description="fdsa" title="fas" onConfirm={() => {}}>
+    <Popup
+      buttonDirection="column"
+      cancelText="tesaaaa"
+      confirmText="test"
+      description="fdsa"
+      title="fas"
+      onConfirm={() => {}}
+    >
       <div>test</div>
     </Popup>
     {pipe(
       SVG_ICON_MAP,
       keys,
       map((key) => <Icon color="orange1" key={key} name={key} size="xxl" />),
-      toArray
+      toArray,
     )}
   </div>
 );
 
 const Home = () => (
-  <AsyncBoundary errorFallback={ErrorFallback} loadingFallback={LoadingComponent}>
-    <Skeleton height="sm" width="full" />
-    <Skeleton borderRadius="md" height="lg" width="lg" />
-    <BottomTab />
-  </AsyncBoundary>
+  <Layout showBottomTab headerProps={{ title: "홈" }}>
+    <AsyncBoundary
+      errorFallback={ErrorFallback}
+      loadingFallback={LoadingComponent}
+    >
+      <Skeleton height="sm" width="full" />
+      <Skeleton borderRadius="md" height="lg" width="lg" />
+    </AsyncBoundary>
+  </Layout>
 );
 
 export default Home;
