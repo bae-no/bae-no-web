ec722357fa7c2d3d4ceefae55c05a49b2f522f52
lifeisegg123 <leejj2002@naver.com>
2022-07-31 17:44:47 +0900

    add: preset

--- lighthouserc.js
+++ lighthouserc.js
@@ -8,5 +8,13 @@ module.exports = {
     collect: {
       url: "http://localhost:3000",
     },
+    assert: {
+      preset: "lighthouse:no-pwa",
+      assertions: {
+        "offscreen-images": "off",
+        "color-contrast": "off",
+        "tap-targets": "off",
+      },
+    },
   },
 };
