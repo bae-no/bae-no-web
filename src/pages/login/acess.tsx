909761eca38393fa14978329db0b2072035fe514
LeeSeungHun <dos07008@naver.com>
2022-09-14 01:03:59 +0900

    CI 수정

--- src/pages/login/acess.tsx
+++ src/pages/login/acess.tsx
@@ -1,17 +1,3 @@
-import { useEffect } from "react";
-
-const Acess = () => {
-  useEffect(() => {
-    if (typeof window === "undefined") return;
-    const getKakaoToken = new URLSearchParams(window.location.search).get(
-      "code",
-    );
-    const getGoogleToken = new URLSearchParams(
-      window.location.hash.substring(1),
-    ).get("access_token");
-    console.log(getKakaoToken, getGoogleToken);
-  }, []);
-  return <div>acess 페이지</div>;
-};
+const Acess = () => <div>acess 페이지</div>;
 
 export default Acess;
