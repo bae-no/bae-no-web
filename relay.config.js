3770d9d450176f7d6f03a94614503d2d70d9f12c
EUNYUSEO <eyseo328@gmail.com>
2022-10-03 22:41:28 +0900

    chore: urql

--- relay.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  artifactDirectory: "src/queries/__generated__",
-  exclude: ["**/node_modules/**", "**/__mocks__/**", "**/__generated__/**"],
-  extensions: ["ts", "tsx"],
-  language: "typescript",
-  schema: "./src/schema/__generated__/schema.graphql",
-  src: "./src",
-};
