82338d7079f0b30227c1d3057b1b12d5342abe61
Eunyu Seo <87485508+EUNYUSEO@users.noreply.github.com>
2022-10-09 11:54:23 +0900

    urql 세팅 적용 & recoil, react-naver-map 설치

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,34 +1,27 @@
 import Head from "next/head";
-import { RelayEnvironmentProvider } from "react-relay/hooks";
-import { getInitialPreloadedQuery, getRelayProps } from "relay-nextjs/app";
+import { RecoilRoot } from "recoil";
 
 import { useInit } from "src/hooks";
-import { getClientEnvironment } from "src/utils/relay/client_environment";
 
 import type { AppProps } from "next/app";
 
 import "src/ui/global.css";
 
-const clientEnv = getClientEnvironment();
-const initialPreloadedQuery = getInitialPreloadedQuery({
-  createClientEnvironment: () => getClientEnvironment()!,
-});
-
 const MyApp = ({ Component, pageProps }: AppProps) => {
-  const relayProps = getRelayProps(pageProps, initialPreloadedQuery);
-  const env = relayProps.preloadedQuery?.environment ?? clientEnv!;
-
   useInit();
+
   return (
-    <RelayEnvironmentProvider environment={env}>
+    <>
       <Head>
         <meta
           content="width=device-width, initial-scale=1, maximum-scale=1.0, minimum-scale=1, user-scalable=0, viewport-fit=cover"
           name="viewport"
         />
       </Head>
-      <Component {...pageProps} {...relayProps} />
-    </RelayEnvironmentProvider>
+      <RecoilRoot>
+        <Component {...pageProps} />
+      </RecoilRoot>
+    </>
   );
 };
 
