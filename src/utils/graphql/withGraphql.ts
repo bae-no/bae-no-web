c9fa5ad8e32045d84bcd52063a5cf395edd5f754
lifeisegg <leejj2002@naver.com>
2022-10-30 23:07:24 +0900

    feat: urql ssr 세팅 (#200)
    Co-authored-by: EUNYUSEO <eyseo328@gmail.com>

--- src/utils/graphql/withGraphql.ts
+++ src/utils/graphql/withGraphql.ts
@@ -1,61 +1,24 @@
-import { authExchange } from "@urql/exchange-auth";
+import { reastorage } from "@reastorage/react";
 import { withUrqlClient } from "next-urql";
-import {
-  makeOperation,
-  dedupExchange,
-  cacheExchange,
-  fetchExchange,
-} from "urql";
+import { cacheExchange, dedupExchange, fetchExchange } from "urql";
 
-export const withGraphql = withUrqlClient(
-  (ssrExchange) => ({
-    exchanges: [
-      authExchange<{ token: string }>({
-        addAuthToOperation: ({ authState, operation }) => {
-          if (!authState || !authState.token) {
-            return operation;
-          }
+import { authExchange } from "./authExchange";
 
-          const fetchOptions =
-            typeof operation.context.fetchOptions === "function"
-              ? operation.context.fetchOptions()
-              : operation.context.fetchOptions || {};
-          return makeOperation(operation.kind, operation, {
-            ...operation.context,
-            fetchOptions: {
-              ...fetchOptions,
-              headers: {
-                ...fetchOptions.headers,
-                Authorization: authState.token,
-              },
-            },
-          });
-        },
-        didAuthError: ({ error }) =>
-          error.graphQLErrors.some((e) => e.extensions?.code === "FORBIDDEN"),
-        getAuth: async ({ authState }) => {
-          if (!authState) {
-            // TODO: localStorage token 가져오기 (임시로 token 넣으면 가능)
-            // const token = localStorage.getItem("token");
-            const token = "access token 입력";
+// FIXME: 위치 변경 필요
+export const accessTokenStorage = reastorage("accessToken", "");
 
-            return {
-              token: `Bearer ${token}`,
-            };
-          }
+export const withGraphql = withUrqlClient((ssr) => ({
+  exchanges: [
+    ssr,
+    dedupExchange,
+    cacheExchange,
+    authExchange(() => {
+      if (typeof window === "undefined") return "";
 
-          return null;
-        },
-      }),
-      dedupExchange,
-      cacheExchange,
-      ssrExchange,
-      fetchExchange,
-    ],
-    suspense: true,
-    url: process.env.NEXT_PUBLIC_SERVER_URL as string,
-  }),
-  {
-    ssr: true,
-  },
-);
+      return accessTokenStorage.get();
+    }),
+    fetchExchange,
+  ],
+  suspense: true,
+  url: process.env.NEXT_PUBLIC_SERVER_URL as string,
+}));
