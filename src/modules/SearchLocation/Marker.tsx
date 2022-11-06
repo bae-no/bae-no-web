78663c7608b45935ad4333efb7eaa3439dcef144
LeeSeungHun <dos07008@naver.com>
2022-11-06 17:04:07 +0900

    getPosition 함수 useEffect 안에 넣기

--- src/modules/SearchLocation/Marker.tsx
+++ src/modules/SearchLocation/Marker.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useEffect } from "react";
+import { useEffect } from "react";
 
 import { useCenter } from "@r2don/react-naver-map";
 
@@ -20,50 +20,45 @@ export const Marker = ({
 }: MarkerProps) => {
   const { setCenter } = useCenter();
 
-  const getPosition = useCallback(() => {
-    window.naver.maps.Service.geocode(
-      {
-        query: location.roadAddress ?? location.jibunAddress,
-      },
-      (status: number, response: any) => {
-        if (status === 200) {
-          const [latitude, longitude] = [
-            response.v2.addresses[0].y,
-            response.v2.addresses[0].x,
-          ];
-          setCenter({
-            latitude,
-            longitude,
-          });
-          getLocationAddress();
-          return;
-        }
-        if (status === 400) {
-          throw new Error("Bad Request Exception");
-        }
-        if (status === 500) {
-          throw new Error("Unexpected Error");
-        }
-      },
-    );
-  }, [
-    location.roadAddress,
-    location.jibunAddress,
-    setCenter,
-    getLocationAddress,
-  ]);
-
   useEffect(() => {
     if (!location.jibunAddress && !location.roadAddress) {
       getLocationThroughCurrentPosition();
       return;
     }
+    const getPosition = () => {
+      window.naver.maps.Service.geocode(
+        {
+          query: location.roadAddress ?? location.jibunAddress,
+        },
+        (status: number, response: any) => {
+          if (status === 200) {
+            const [latitude, longitude] = [
+              response.v2.addresses[0].y,
+              response.v2.addresses[0].x,
+            ];
+            setCenter({
+              latitude,
+              longitude,
+            });
+            getLocationAddress();
+            return;
+          }
+          if (status === 400) {
+            throw new Error("Bad Request Exception");
+          }
+          if (status === 500) {
+            throw new Error("Unexpected Error");
+          }
+        },
+      );
+    };
     getPosition();
   }, [
-    getPosition,
+    getLocationAddress,
     getLocationThroughCurrentPosition,
     location.jibunAddress,
     location.roadAddress,
+    setCenter,
   ]);
   return (
     <Box
