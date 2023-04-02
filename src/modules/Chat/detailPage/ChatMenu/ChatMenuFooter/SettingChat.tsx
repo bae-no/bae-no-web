f7099d1deb1ee3b7beeaab032a6b13144e57dd19
LeeSeungHun <dos07008@naver.com>
2023-04-02 16:10:15 +0900

    ci에러 수정

--- src/modules/Chat/detailPage/ChatMenu/ChatMenuFooter/SettingChat.tsx
+++ src/modules/Chat/detailPage/ChatMenu/ChatMenuFooter/SettingChat.tsx
@@ -2,15 +2,11 @@ import { Box } from "src/ui/Box";
 import { Icon } from "src/ui/Icon";
 import { Typography } from "src/ui/Typography";
 
-export const SettingChat = () => {
-  const a = "a";
-
-  return (
-    <Box as="button" flexDirection="row" gap="8">
-      <Icon name="setting" />
-      <Typography color="black2" fontSize="body2-m">
-        채팅방 설정
-      </Typography>
-    </Box>
-  );
-};
+export const SettingChat = () => (
+  <Box as="button" flexDirection="row" gap="8">
+    <Icon name="setting" />
+    <Typography color="black2" fontSize="body2-m">
+      채팅방 설정
+    </Typography>
+  </Box>
+);
