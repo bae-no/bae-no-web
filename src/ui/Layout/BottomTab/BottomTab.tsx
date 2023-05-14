54d6a6205d114068898afeb594dbd725d1188691
lifeisegg123 <leejj2002@naver.com>
2023-05-14 20:42:42 +0900

    chore: remove prefetch

--- src/ui/Layout/BottomTab/BottomTab.tsx
+++ src/ui/Layout/BottomTab/BottomTab.tsx
@@ -1,7 +1,4 @@
-import { useEffect } from "react";
-
 import Link from "next/link";
-import { useRouter } from "next/router";
 
 import { Box } from "src/ui/Box";
 import { Icon } from "src/ui/Icon";
@@ -13,46 +10,38 @@ import {
 } from "./BottomTab.css";
 import BottomTabItem from "./BottomTabItem";
 
-const bottomTabRoutes = ["/", "/search", "/chat/create", "/chat", "/user"];
-
-const BottomTab = () => {
-  const router = useRouter();
-  useEffect(() => {
-    bottomTabRoutes.forEach((route) => router.prefetch(route));
-  }, [router]);
-  return (
-    <Box as="nav" className={bottomTabContainerCss}>
+const BottomTab = () => (
+  <Box as="nav" className={bottomTabContainerCss}>
+    <Box
+      backgroundColor="white"
+      borderTopRadius="24"
+      bottom="0"
+      className={bottomTabCss}
+      flexDirection="row"
+      gap="4"
+      justify="space-between"
+      paddingBottom="24"
+      paddingTop="8"
+      position="fixed"
+      px="16"
+      width="full"
+    >
+      <BottomTabItem href="/" iconName="home" tabName="홈" />
+      <BottomTabItem href="/search" iconName="search" tabName="검색" />
       <Box
-        backgroundColor="white"
-        borderTopRadius="24"
-        bottom="0"
-        className={bottomTabCss}
-        flexDirection="row"
-        gap="4"
-        justify="space-between"
-        paddingBottom="24"
-        paddingTop="8"
-        position="fixed"
-        px="16"
-        width="full"
+        as={Link}
+        className={addIconCss}
+        href="/chat/create"
+        position="relative"
+        title="채팅방 생성"
+        top="0"
       >
-        <BottomTabItem href="/" iconName="home" tabName="홈" />
-        <BottomTabItem href="/search" iconName="search" tabName="검색" />
-        <Box
-          as={Link}
-          className={addIconCss}
-          href="/chat/create"
-          position="relative"
-          title="채팅방 생성"
-          top="0"
-        >
-          <Icon name="add" />
-        </Box>
-        <BottomTabItem href="/chat" iconName="chat" tabName="채팅" />
-        <BottomTabItem href="/user" iconName="user" tabName="내 정보" />
+        <Icon name="add" />
       </Box>
+      <BottomTabItem href="/chat" iconName="chat" tabName="채팅" />
+      <BottomTabItem href="/user" iconName="user" tabName="내 정보" />
     </Box>
-  );
-};
+  </Box>
+);
 
 export default BottomTab;
